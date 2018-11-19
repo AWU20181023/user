bf19c864c5b91868dfd154724dd4092fb75bf64e
AWU20181023 <18578275545@163.com>
2018-11-19 14:02:45 +0800

    多模块改造

---
+++ user-server/src/main/java/com/gree/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.gree.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 260152(AWU) on 2018/11/19 14:02.
+ */
+@RestController
+public class UserController {
+
+}
